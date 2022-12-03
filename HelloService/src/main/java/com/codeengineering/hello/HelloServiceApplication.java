8630aa3fbf5d87b36e69fd48f6456ca28f25c3e0
code-engineering <code.engineering.tech@gmail.com>
2022-12-03 14:29:29 -0800

    Ep02 - Add Spring Boot (Basic Framework)
    * SpringBoot to provide basic framework
    * i.e. dependency injection, metric, logging

---
+++ HelloService/src/main/java/com/codeengineering/hello/HelloServiceApplication.java
@@ -0,0 +1,11 @@
+package com.codeengineering.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServiceApplication.class, args);
+    }
+}
--- HelloService/src/main/java/com/codeengineering/hello/HelloServiceImpl.java
+++ HelloService/src/main/java/com/codeengineering/hello/HelloServiceImpl.java
@@ -4,7 +4,9 @@ import com.codeengineering.hello.protobuf.HelloRequest;
 import com.codeengineering.hello.protobuf.HelloResponse;
 import com.codeengineering.hello.protobuf.HelloServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
+@GrpcService
 public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
     @Override
     public void greetings(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
